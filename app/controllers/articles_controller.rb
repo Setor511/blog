44bb1c5e886423c79dcdbabd403fd061c7bb2279
Hector <setor51montaurf@gmail.com>
2024-10-31 00:25:57 -0300

    tercer commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,29 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show
+    # params = {id: 1}
+    @article = Article.find(params[:id])
+  end
+
+  # muestra el formulario
+  def new
+      @article = Article.new
+  end
+
+  # toma los params ya filtrados y crea el artículo: si se crea correctamente, lo muestro, si falla, muestro los errores en el mismo new
+  def create
+      @article = Article.new(articles_params)
+      if @article.save
+        redirect_to @article
+      else
+        render :new, status: :unprocessable_entity
+      end
+   end
+
+  private
+    def articles_params
+      params.require(:article).permit(:title, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
 
-  get "/articles", to: "articles#index"
-
+  #get "/articles", to: "articles#index"
+  #get "/articles/:id", to: "articles#show"
+  resources :articles
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
