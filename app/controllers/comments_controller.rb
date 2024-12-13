41e005cd65ff2b9959f50cabba3bc3bfeb0903a3
Hector <setor51montaurf@gmail.com>
2024-12-13 00:26:56 -0300

    usuarios/autenticacion

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,14 @@
 class CommentsController < ApplicationController
-    def create
-      @article = Article.find(params[:article_id])
-      @comment = @article.comments.create(comment_params)
-      redirect_to article_path(@article)
-    end
-  
+  def create
+    @article = Article.find(params[:article_id])
+    # Crear el comentario asociado al usuario logueado
+    @comment = @article.comments.create(comment_params.merge(user_id: current_user.id))
+    # @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
     private
-  
+
     def comment_params
-      params.require(:comment).permit(:commenter, :body)
+     params.require(:comment).permit(:commenter, :body)
     end
 end
